69b4ae2e0b354581d9af1f9a9d864cd431b2a73f
Oficina Caçapava 350 <oficicacapava350@outlook.com>
2021-04-08 17:12:37 -0300

    up

--- Skynet/src/app/app-routing.module.ts
+++ Skynet/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DicasComponent } from './dicas/dicas.component';
 
-const routes: Routes = [];
+
+
+
+
+const routes: Routes = [
+  { path: 'dicas', component: DicasComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
