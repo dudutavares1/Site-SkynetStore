8c865e468d52911881737020abe54cc334031492
Oficina Caçapava 350 <oficicacapava350@outlook.com>
2021-04-08 11:22:44 -0300

    up paginas menu

--- Skynet/src/app/app.module.ts
+++ Skynet/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DicasComponent } from './dicas/dicas.component';
+import { CrosshairComponent } from './crosshair/crosshair.component';
+import { TeamComponent } from './team/team.component';
+import { ArmasComponent } from './loja/armas/armas.component';
+import { FacasComponent } from './loja/facas/facas.component';
+import { LuvasComponent } from './loja/luvas/luvas.component';
+import { PromocaoComponent } from './loja/promocao/promocao.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DicasComponent,
+    CrosshairComponent,
+    TeamComponent,
+    ArmasComponent,
+    FacasComponent,
+    LuvasComponent,
+    PromocaoComponent
   ],
   imports: [
     BrowserModule,
---
+++ Skynet/src/app/crosshair/crosshair.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crosshair',
+  templateUrl: './crosshair.component.html',
+  styleUrls: ['./crosshair.component.scss']
+})
+export class CrosshairComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
