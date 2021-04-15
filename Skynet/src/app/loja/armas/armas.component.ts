8d1f0d734eec0faaccc72c20e8081d9367cb5f9c
Oficina Caçapava 350 <oficicacapava350@outlook.com>
2021-04-15 15:15:43 -0300

    up

--- Skynet/src/app/loja/armas/armas.component.ts
+++ Skynet/src/app/loja/armas/armas.component.ts
@@ -7,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArmasComponent implements OnInit {
 
-  team: any = [
+  loja: any = [
   {
- name: "Dudu Tavares",
- avatar: "https://scontent.fsjk2-1.fna.fbcdn.net/v/t1.6435-9/121113910_391132525606554_1134199343421530156_n.jpg?_nc_cat=111&ccb=1-3&_nc_sid=84a396&_nc_ohc=crwXyGjPJQgAX-GEoF9&_nc_ht=scontent.fsjk2-1.fna&oh=5f1b0a55df9f93b8eeb447dbe9f6c2a5&oe=609F8174",
+ name: "Ak",
+ preco: "400,00",
+ Condicao: "Nova de fabrica",
+
+ avatar: "https://csgostash.com/storage/img/skin_sideview/s1285.png?id=4816ffb3a25e858c60dc",
 },
 
 
