852e4a3c175948ed59af21f353ee16a4cdd60a71
dudutavares <80931058+dudutavares@users.noreply.github.com>
2021-04-18 17:19:17 -0300

    videos
    videos

--- Skynet/src/app/dicas/dicas.component.ts
+++ Skynet/src/app/dicas/dicas.component.ts
@@ -6,6 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dicas.component.scss']
 })
 export class DicasComponent implements OnInit {
+videolist=[
+  {
+    name: "nome",
+    desc:"titulo",
+    Link :"https://www.youtube.com/watch?v=gHPPAdS3o4M&ab_channel=e-SporTV",
+  }
+]
+
 
   constructor() { }
 
