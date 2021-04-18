4ba46855a04c5d5717ddbaef7de836a3f30031a5
dudutavares <80931058+dudutavares@users.noreply.github.com>
2021-04-18 19:01:37 -0300

    up

--- Skynet/src/app/dicas/dicas.component.ts
+++ Skynet/src/app/dicas/dicas.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dicas.component.scss']
 })
 export class DicasComponent implements OnInit {
-videolist=[
+  dicas: any = [
   {
-    name: "nome",
-    desc:"titulo",
+
+    avatar: "https://i.ytimg.com/vi/gHPPAdS3o4M/maxresdefault.jpg",
+    titulo: "Fallen dicas de AWP",
+    Link :"https://www.youtube.com/watch?v=gHPPAdS3o4M&ab_channel=e-SporTV",
+  }
+]
+
+dica2s: any = [
+  {
+
+    avatar: "https://i.ytimg.com/vi/gHPPAdS3o4M/maxresdefault.jpg",
+    titulo: "Fallen dicas de AWP",
     Link :"https://www.youtube.com/watch?v=gHPPAdS3o4M&ab_channel=e-SporTV",
   }
 ]
