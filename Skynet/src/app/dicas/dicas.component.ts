98e50941d4bf61636afe03718084682deeee22a6
dudutavares <80931058+dudutavares@users.noreply.github.com>
2021-04-18 21:59:19 -0300

    up videos
    up

--- Skynet/src/app/dicas/dicas.component.ts
+++ Skynet/src/app/dicas/dicas.component.ts
@@ -7,21 +7,80 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DicasComponent implements OnInit {
   dicas: any = [
-  {
 
-    avatar: "https://i.ytimg.com/vi/gHPPAdS3o4M/maxresdefault.jpg",
+    //falllen strafe Fino
+    {
+    avatar: "https://i.ytimg.com/vi/7SoUukz13sU/maxresdefault.jpg",
     titulo: "Fallen dicas de AWP",
-    Link :"https://www.youtube.com/watch?v=gHPPAdS3o4M&ab_channel=e-SporTV",
-  }
-]
+    Link :"https://www.youtube.com/watch?v=7SoUukz13sU&ab_channel=e-SporTV",
 
-dica2s: any = [
-  {
 
-    avatar: "https://i.ytimg.com/vi/gHPPAdS3o4M/maxresdefault.jpg",
-    titulo: "Fallen dicas de AWP",
-    Link :"https://www.youtube.com/watch?v=gHPPAdS3o4M&ab_channel=e-SporTV",
-  }
+    // Fino Straffe
+    avatar2: "https://i.ytimg.com/vi/gHPPAdS3o4M/maxresdefault.jpg",
+    titulo2: "Dicas de Strafe e AWP no CSGO",
+    Link2 :"https://www.youtube.com/watch?v=FEd_w8_UNag&ab_channel=e-SporTV",
+
+
+    // smoke
+    avatar3: "https://i.ytimg.com/vi/xxhhTGNZ5UE/maxresdefault.jpg",
+    titulo3: "Dicas de Strafe e AWP no CSGO",
+    Link3 :"https://www.youtube.com/watch?v=xxhhTGNZ5UE&ab_channel=e-SporTV",
+
+    // AWP Train
+
+    avatar4: "https://i.ytimg.com/vi/3mcYo_EaYj4/maxresdefault.jpg",
+    titulo4: "Dicas de Strafe e AWP no CSGO",
+    Link4 :"https://www.youtube.com/watch?v=3mcYo_EaYj4&ab_channel=e-SporTV",
+
+    // diferentes granadas
+    avatar5: "https://i.ytimg.com/vi/hocYvvQIdkk/maxresdefault.jpg",
+    titulo5: "Dicas de Strafe e AWP no CSGO",
+    Link5 :"https://www.youtube.com/watch?v=hocYvvQIdkk&ab_channel=e-SporTV",
+
+
+ // smoke guardian
+    avatar6: "https://i.ytimg.com/vi/kicskgg-yjs/maxresdefault.jpg",
+    titulo6: "Dicas de Strafe e AWP no CSGO",
+    Link6 :"https://www.youtube.com/watch?v=kicskgg-yjs&ab_channel=e-SporTV",
+
+    //dominio banana
+    avatar7: "https://i.ytimg.com/vi/Ssr6uZY2Z9Q/maxresdefault.jpg",
+    titulo7: "Dicas de Strafe e AWP no CSGO",
+    Link7 :"https://www.youtube.com/watch?v=Ssr6uZY2Z9Q&ab_channel=e-SporTV",
+
+    // jumpthrow
+    avatar8: "https://i.ytimg.com/vi/7KsMQvLwNaA/maxresdefault.jpg",
+    titulo8: "Dicas de Strafe e AWP no CSGO",
+    Link8 :"https://www.youtube.com/watch?v=7KsMQvLwNaA&ab_channel=e-SporTV",
+
+
+// flash
+    avatar9: "https://s02.video.glbimg.com/x240/6597549.jpg",
+    titulo9: "Dicas de Strafe e AWP no CSGO",
+    Link9 :"https://www.youtube.com/watch?v=9MIduNFBCOQ&ab_channel=GamersClubMedia",
+
+
+    //ak-47
+    avatar10: "https://i.ytimg.com/vi/J-Yu0fcw6Uk/maxresdefault.jpg",
+    titulo10: "Dicas de Strafe e AWP no CSGO",
+    Link10 :"https://www.youtube.com/watch?v=J-Yu0fcw6Uk&ab_channel=e-SporTV",
+
+
+// recoil
+    avatar11: "https://i.ytimg.com/vi/1BT_G3-ssEY/sddefault.jpg",
+    titulo11: "Dicas de Strafe e AWP no CSGO",
+    Link11 :"https://www.youtube.com/watch?v=1BT_G3-ssEY&ab_channel=fallenINSIDER",
+
+
+    //dicas verdadeiro
+    avatar12: "https://i.ytimg.com/vi/g7YSdiJtj6U/mqdefault.jpg",
+    titulo12: "Dicas de Strafe e AWP no CSGO",
+    Link12 :"https://www.youtube.com/watch?v=g7YSdiJtj6U&ab_channel=fallenINSIDER",
+
+  },
+
+
+
 ]
 
 
